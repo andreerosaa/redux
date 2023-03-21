e13e03d19e6959e0854b27dec3dc51ae843c8073
André Santos Rosa <arossant@emeal.nttdata.com>
2023-03-21 17:54:35 +0000

    new react application

---
+++ src/NewPost.js
@@ -0,0 +1,27 @@
+const NewPost = (
+  {postTitle, setPostTitle, postBody, setPostBody, handleSubmit}
+) => {
+    return (
+      <main className="New Post">
+          <h2>NewPost</h2>
+          <form className="newPostForm" onSubmit={handleSubmit}>
+            <label htmlFor="postTitle">Title:</label>
+            <input
+              id="postTitle"
+              type="text"
+              required
+              value={postTitle}
+              onChange={(e) => setPostTitle(e.target.value)}/>
+            <label htmlFor="postBody">post:</label>
+            <textarea
+              id="postBody"
+              required
+              value = {postBody}
+              onChange={(e) => setPostBody(e.target.value)}/>
+            <button type="submit">Submit</button>
+          </form>
+      </main>
+    )
+  }
+  
+  export default NewPost
