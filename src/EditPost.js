28221650309334da7a1738550a1483ef739405b3
André Santos Rosa <arossant@emeal.nttdata.com>
2023-03-22 12:00:04 +0000

    added al CRUD operations functionalities with axios and local json server

---
+++ src/EditPost.js
@@ -0,0 +1,56 @@
+import { useEffect } from "react"
+import { useParams, Link } from "react-router-dom"
+
+const EditPost = ({posts, handleEdit, editBody, setEditBody, editTitle, setEditTitle}) => {
+
+    const {id} = useParams();
+    console.log(id)
+    const post = posts.find(post => (post.id).toString() === id);
+    
+
+    useEffect(() => {
+        if(post){
+            setEditTitle(post.title)
+            setEditBody(post.body)
+        }
+    }, [post, setEditTitle, setEditBody])
+
+return (
+    <main className="New Post">
+        {editTitle &&
+            <>
+                <h2>Edit Post</h2>
+                <form className="newPostForm" onSubmit={(e) => e.preventDefault()}>
+                    <label htmlFor="postTitle">Title:</label>
+                    <input
+                    id="postTitle"
+                    type="text"
+                    required
+                    value={editTitle}
+                    onChange={(e) => setEditTitle(e.target.value)}/>
+                    <label htmlFor="postBody">post:</label>
+                    <textarea
+                    id="postBody"
+                    required
+                    value = {editBody}
+                    onChange={(e) => setEditBody(e.target.value)}/>
+                    <button type="submit" onClick={() => {
+                        handleEdit(post.id)
+                    }}>Edit</button>
+                </form>
+            </>
+        }
+        {!post &&
+            <>
+                <h2>Post Not Found</h2>
+                <p>Well, that's disappointing.</p>
+                <p>
+                    <Link to='/'>Visit Our Homepage</Link>
+                </p>
+            </>
+        }
+    </main>
+  )
+}
+
+export default EditPost
